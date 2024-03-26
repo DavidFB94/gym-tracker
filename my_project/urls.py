a39e36455d21366611d515953602b43cdce35e3f
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-03-26 11:04:21 +0000

    Create home app with url

--- my_project/urls.py
+++ my_project/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from hello_world import views as index_views
+from home import views as home_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path('index/', index_views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('home/', home_views.home_page, name='home'),
 ]
