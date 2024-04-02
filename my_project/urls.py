1f948b9f81c9f9b86e627bc3ae6c34e2ac55469b
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-04-02 12:15:51 +0000

    Adjust urls for hello_world and home

--- my_project/urls.py
+++ my_project/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from hello_world import views as index_views
+from hello_world import views as hello_views
 from home import views as home_views
 
 urlpatterns = [
-    path('index/', index_views.index, name='index'),
+    path('hello/', hello_views.hello, name='hello_world'),
     path('admin/', admin.site.urls),
-    path('home/', home_views.home_page, name='home'),
+    path('', home_views.home_page, name='home'),
 ]
