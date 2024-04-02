b9ae3f2e029e6ed07604d9e39071a86c2371400e
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-04-02 11:24:21 +0000

    Add Workout model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Workout(models.Model):
+    """
+    Model for user workout tracking.
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    note = models.TextField(null=False, blank=False)
+    date = models.DateField(null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
