b65a5abad91cd03d9ac74ce853ef5988d38f12e4
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-04-16 13:44:21 +0000

    Create "add workout" view

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Workout
+from .forms import WorkoutForm
 
 
 def home(request):
@@ -13,3 +15,20 @@ def home(request):
         "object_list": queryset,
     }
     return render(request, template, context)
+
+
+def add_workout(request):
+    workout_form = WorkoutForm(request.POST or None)
+    if request.method == "POST":
+        if workout_form.is_valid():
+            workout_form.instance.user = request.user
+            workout_form.save()
+            messages.success(request, "Workout Added!")
+            return redirect(reverse("home"))
+    template = "home/add_workout.html"
+    context = {
+        "workout_form": workout_form,
+    }
+    return render(request, template, context)
+
+
