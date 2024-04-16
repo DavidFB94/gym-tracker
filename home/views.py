761432c60f35c9663b0bfdb00ba10640c4fd5d6d
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-04-16 09:49:46 +0000

    Adjust home view to display content based on user

--- home/views.py
+++ home/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Workout
 
-# Create your views here.
-class HomePage(generic.ListView):
-    queryset = Workout.objects.all()
-    template_name = "home/home.html"
+
+def home(request):
+    if request.user.is_authenticated:
+        queryset = Workout.objects.filter(user=request.user)
+    else:
+        queryset = []
+    template = "home/home.html"
+    context = {
+        "object_list": queryset,
+    }
+    return render(request, template, context)
