c877cddd0eb4c135b1111cb967c8a86c0a11f48f
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-04-16 13:25:51 +0000

    Add urls for "add-workout" and "add-exercise"

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('add-workout/', views.add_workout, name='add-workout'),
+    path('add-exercise/', views.add_exercise, name='add-exercise'),
 ]
