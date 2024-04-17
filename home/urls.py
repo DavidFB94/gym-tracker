8791b1b403740d95ebe727932e61433a3c55a71f
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-04-17 15:15:56 +0000

    Create delete_exercise url

--- home/urls.py
+++ home/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('edit_workout/<int:id>/', views.edit_workout, name='edit_workout'),
     path('edit_exercise/<int:id>/', views.edit_exercise, name='edit_exercise'),
     path('delete_workout/<int:id>/', views.delete_workout, name='delete_workout'),
+    path('delete_exercise/<int:id>/', views.delete_exercise, name='delete_exercise'),
 ]
