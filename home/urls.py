1f09e58274cd5303870e1cbbf15ebe5822b9a787
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-04-17 12:19:45 +0000

    Create edit_exercise url

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add_workout/', views.add_workout, name='add_workout'),
     path('add_exercise/<int:id>/', views.add_exercise, name='add_exercise'),
     path('edit_workout/<int:id>/', views.edit_workout, name='edit_workout'),
+    path('edit_exercise/<int:id>/', views.edit_exercise, name='edit_exercise'),
 ]
