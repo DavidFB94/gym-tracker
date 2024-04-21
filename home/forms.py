4041f54f3fa8d7ec5fadbddba1780205dcb35ec2
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-04-21 17:03:15 +0000

    Adjust form content with django widgets

--- home/forms.py
+++ home/forms.py
@@ -5,12 +5,27 @@ from .models import Exercise
 
 #Create forms here
 class WorkoutForm(ModelForm):
+    name = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control',
+    'placeholder': 'Workout name'}))
+    note = forms.CharField(label='', widget=forms.Textarea(attrs={'class': 'form-control',
+    'placeholder': 'Note.... (optional)', 'rows': 5}), required=False)
+    date = forms.DateField(label='', widget=forms.DateInput(attrs={'type': 'date'}))
+
     class Meta:
         model = Workout
         fields = ('name', 'note', 'date',)
 
 
 class ExerciseForm(ModelForm):
+    name = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control',
+    'placeholder': 'Exercise name'}))
+    weight = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control',
+    'placeholder': 'Weight'}))
+    sets = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control',
+    'placeholder': 'Sets'}))
+    reps = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control',
+    'placeholder': 'Reps',}))
+
     class Meta:
         model = Exercise
         fields = ('name', 'weight', 'sets', 'reps',)
